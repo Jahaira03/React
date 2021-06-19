59f2f9a86374cbb89a8c28837ada7f62816a1053
Jahaira_Cortés <jahairagonzalez8@gmail.com>
2021-06-19 00:32:19 -0500

    Todo

---
+++ hoja_vidacamila/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Principal from '../src/components/Principal'
+import '../src/utils/css/Principal.estilos.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Principal />
+    </div>
+  );
+}
+
+export default App;
